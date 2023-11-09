1f46eb9dd1f84b38f4f33500cbb803ffc5ce92d0
Berkay Anduv <sygrok@hotmail.com>
2023-11-10 02:20:48 +0300

    DerivingValues&OutputtingQuestions&Registering

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header";
+import Quiz from "./components/Quiz";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Quiz />
+    </>
+  );
 }
 
 export default App;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,28 @@
 import { useState } from "react";
+import QUESTIONS from "../question.js";
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
 
-  return <p>Currently active Question</p>;
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevData) => {
+      return [...prevData, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((x) => (
+            <li className="answer" key={x}>
+              <button onClick={() => handleSelectAnswer(x)}>{x}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
