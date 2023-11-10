7ca2af103ff7e47f5df40f0dea0620d3339a49a8
Berkay Anduv <sygrok@hotmail.com>
2023-11-10 03:00:11 +0300

    ShufflingAnswers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 import QUESTIONS from "../question.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} /> <h2>Quiz is complete!</h2>
+      </div>
+    );
+  }
+
+  // shuffledAnswers should run after if check in order to prevent crash
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5); //Build in method 'sort()'
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevData) => {
@@ -16,7 +30,7 @@ export default function Quiz() {
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((x) => (
+          {shuffledAnswers.map((x) => (
             <li className="answer" key={x}>
               <button onClick={() => handleSelectAnswer(x)}>{x}</button>
             </li>
